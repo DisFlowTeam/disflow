a3955bb9213f5ea553030e62fe4c16d35e93d3f6
retrouser955 <70205403+retrouser955@users.noreply.github.com>
2025-05-23 12:49:21 +0630

    refactor: BaseNode added protected methods and change BaseNode to an abstract class

--- packages/nodes/src/nodes/BaseNode.ts
+++ packages/nodes/src/nodes/BaseNode.ts
@@ -1,7 +1,11 @@
-import { LGraphNode } from "litegraph.js";
+import { type INodeOutputSlot, LGraphNode } from "litegraph.js";
 import type { GenerationContext } from "@disflow-team/code-gen"
 
-export class BaseNode extends LGraphNode {
+export interface BaseNode {
+    onOutputAdded?: (output: INodeOutputSlot) => any;
+}
+
+export abstract class BaseNode extends LGraphNode {
     constructor() {
         super();
         this.builder();
@@ -19,23 +23,36 @@ export class BaseNode extends LGraphNode {
      * Set the name of the node
      * @param name the name of the node
      */
-    setName(name: string) {
+    protected setName(name: string) {
         this.title = name;
         return this;
+    }
+
+    protected insertOutput(index: number, name: string, type: string | -1, extraInfo?: INodeOutputSlot) {
+        if(index > this.outputs.length - 1) throw new Error(`Cannot insert there. Output length is is only ${this.outputs.length}. Maxmium insert is ${this.outputs.length - 1}.`)
+        const output: INodeOutputSlot =  { name, type, links: null };
+        if(extraInfo) {
+            for (const i in extraInfo) {
+                // @ts-expect-error
+                output[i] = extraInfo[i];
+            }
+        }
+
+        if(!this.outputs) this.outputs = [];
+        this.outputs.splice(index, 0, output);
+
+        if(this.onOutputAdded) {
+            this.onOutputAdded(output)
+        }
     }
 
     /**
      * Called when the node is constructed
      */
-    builder() {
-        throw new Error('Not implemented');
-    }
-
+    protected abstract builder(): void;
     /**
      * Called when the node is needed to generate code
      * @param ctx The context of the current generation
      */
-    onGenerateCode(ctx: GenerationContext): string {
-        throw new Error("Not implemented");
-    }
+    abstract onGenerateCode(ctx: GenerationContext): string;
 }
